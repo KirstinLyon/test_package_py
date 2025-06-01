4b949f730a6c69894133af88809af70bd4febd21
kirstinlyon <kirstin@mltwelve.com>
2025-06-01 10:00:11 +0200

    initial commit

---
+++ test_package_py/test_module_1.py
@@ -0,0 +1,10 @@
+def hello_world():
+    """Prints 'Hello, World!' to the console.
+
+    This function is a simple example to demonstrate how to document a function in Python.
+
+    Examples:
+        >>> hello_world()
+        Hello, World!
+    """
+    print("Hello, World!")
